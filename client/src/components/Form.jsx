f4896f0cfa41a342b1cfb67be25f6d4dc80cb4e2
fadoua bach <fadouaba.97@gmail.com>
2026-02-13 23:03:56 +0100

    reate register form UI with controlled inputs

---
+++ client/src/components/Form.jsx
@@ -0,0 +1,88 @@
+import { useState } from "react"
+import axios from "axios"
+import "./Form.css"
+
+
+function Form() {
+
+  const [formData, setFormData] = useState({
+    firstname: "",
+    lastname: "",
+    birthdate: "",
+    email: "",
+    password: "",
+  })
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    console.log(formData)
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>First name:</label>
+        <input
+          type="text"
+          name="firstname"
+          value={formData.firstname}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div>
+        <label>Last name:</label>
+        <input
+          type="text"
+          name="lastname"
+          value={formData.lastname}
+          onChange={handleChange}
+        />
+      </div>
+
+      
+      <div>
+        <label>birthdate:</label>
+        <input
+          type="text"
+          name="birthdate"
+          value={formData.birthdate}
+          onChange={handleChange}
+        />
+      </div>
+
+      
+      <div>
+        <label>email:</label>
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+      </div>
+
+      
+      <div>
+        <label>password:</label>
+        <input
+          type="text"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+      </div>
+
+      <button type="submit">Create</button>
+    </form>
+  )
+}
+
+export default Form
